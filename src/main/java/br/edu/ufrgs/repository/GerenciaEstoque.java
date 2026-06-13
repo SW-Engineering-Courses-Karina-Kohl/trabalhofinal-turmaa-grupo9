5f8bbde6d7978506fc007ae2aad0260045334d06
allanruank <118089711+allanruank@users.noreply.github.com>
2026-06-12 23:08:12 -0300

    Layout + Importação e Exportação

--- src/main/java/br/edu/ufrgs/repository/GerenciaEstoque.java
+++ src/main/java/br/edu/ufrgs/repository/GerenciaEstoque.java
@@ -26,33 +26,28 @@ public class GerenciaEstoque {
 
     public void carregarProdutos(String caminhoArquivo) {
         LeitorCSV leitor = new LeitorCSV(caminhoArquivo);
-        // Utiliza o leitor passando a lista de categorias, conforme a implementação da
-        // equipe
         this.listaProdutos = leitor.lerProdutos(this.listaCategorias);
     }
 
-    public void verificarNecessidadeReposicao() {
+    public void verificarNecessidadeReposicao(String caminhoArquivoSaida) {
         for (Produto produto : listaProdutos) {
             // RF02 - Motor de Reposição: Compara quantidade_atual com o ponto_de_pedido
             if (precisaReposicao(produto)) {
                 int sugestao = calcularSugestaoCompra(produto);
                 String prioridade = definirPrioridade(produto);
 
-                // TODO: Pessoal, descomentem as linhas abaixo quando a classe Produto tiver os
-                // atributos para o RF04!
-                // produto.setSugestaoCompra(sugestao);
-                // produto.setPrioridade(prioridade);
+                // Integrado com a classe Produto (RF04)
+                produto.setSugestaoCompra(sugestao);
+                produto.setPrioridade(prioridade);
             } else {
-                // TODO: Descomentar quando os setters existirem no Produto.java
-                // produto.setSugestaoCompra(0);
-                // produto.setPrioridade("Normal");
+                produto.setSugestaoCompra(0);
+                produto.setPrioridade("Normal");
             }
         }
 
-        // TODO: Ajustar a chamada abaixo quando o GeradorSaidaCSV estiver preparado
-        // para receber a lista
-        // GeradorSaidaCSV gerador = new GeradorSaidaCSV("saida_reposicao.csv");
-        // gerador.exportar(listaProdutos);
+        // Gera o arquivo final com as sugestões de compra
+        GeradorSaidaCSV gerador = new GeradorSaidaCSV(caminhoArquivoSaida);
+        gerador.exportar(listaProdutos);
     }
 
     // --- Métodos Privados (-) ---
